e4ac73cf86f10308452de53f560f797a87d1a996
Julian Chan <julian.chan@mail.com>
2017-03-14 17:25:58 +1100

    added styling; updated html elements; add POST route for user

--- main.rb
+++ main.rb
@@ -2,6 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 require_relative 'database_config'
+require_relative 'models/album'
+require_relative 'models/photo'
+require_relative 'models/user'
+
+enable :sessions
+
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+end
 
 after do
   ActiveRecord::Base.connection.close
@@ -21,4 +36,15 @@ end
 
 get '/photos/:id' do
   erb :photo
+end
+
+post '/users' do
+  user = User.find_by(email: params[:email])
+
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/users/#{user.id}"
+  else
+    redirect '/'
+  end
 end
--- seed.rb
+++ seed.rb
@@ -4,12 +4,12 @@ require_relative 'database_config'
 
 Dir["models/*.rb"].each { |f| require_relative f }
 
-user = User.new(email: "test@account.com", full_name: "John Smith", deleted: FALSE)
+user = User.new(email: "test@account.com", full_name: "John Smith", deleted: false)
 user.password = "test"
 user.save
 
-album = Album.create(name: "Japan trip", description: "Holiday in wintry Tokyo", user_id: user.id, deleted: FALSE)
+album = Album.create(name: "Japan trip", description: "Holiday in wintry Tokyo", user_id: user.id, deleted: false)
 
-photo = Photo.create(url: "https://media-cdn.tripadvisor.com/media/photo-s/07/15/f1/8b/shibuya-crossing.jpg", album_id: album.id, deleted: FALSE)
+photo = Photo.create(url: "https://media-cdn.tripadvisor.com/media/photo-s/07/15/f1/8b/shibuya-crossing.jpg", album_id: album.id, deleted: false)
 
 # binding.pry
